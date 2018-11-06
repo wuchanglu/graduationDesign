53c446b668dce5ad2800018d0c3a4dfcab40c361
SC-201804261133\Administrator <1761221407@qq.com>
2018-11-06 15:19:59 +0800

    项目新起

---
+++ graduation-design/src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+   <div class="component">
+     <div class="component__bolck">123</div>
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.component{
+  &__bolck{
+    background-color: #f00;
+  }
+}
+</style>
